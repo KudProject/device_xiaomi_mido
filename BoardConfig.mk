2c2e2fa3a4af64d35431e3ce17c3fa4482d08360
Albert I <krascgq@outlook.co.id>
2017-09-22 10:47:06 +0800

    mido: Switch SDClang config to one from vendor/aim
    Change-Id: Ia1539579c44ff1ba2b524de4efe63cd45be900ca
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,8 +129,6 @@ ifeq ($(HOST_OS),linux)
   TARGET_USE_SDCLANG := true
   ifeq ($(TARGET_USE_SDCLANG),true)
     SDCLANG := true
-    SDCLANG_PATH := prebuilts/clang/linux-x86/host/sdclang-3.8/bin
-    SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
   endif
 endif
 
