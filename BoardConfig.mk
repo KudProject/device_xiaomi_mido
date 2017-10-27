4353e5d97f2f7ffa9bca85ab99a8643c65f52b1e
Albert I <krascgq@outlook.co.id>
2017-10-27 22:55:57 +0800

    mido: Define kernel toolchain path and prefix
    Change-Id: I17c65007acab6deaeca76ce981f91fed8135e8a4
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,6 +45,8 @@ BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_CONFIG := mido_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-cortex_a53-linux-android-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-cortex_a53-linux-android-7.x/bin
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
